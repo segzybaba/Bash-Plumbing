07893df15a1d881ad761b2d3f6a4e672fb1a4622
segun <segzyguy@gmail.com>
2021-08-05 03:30:16 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const slides = document.querySelectorAll(".slide");
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add("current");
+  } else {
+    slides[0].classList.add("current");
+  }
+  setTimeout(() => current.classList.remove("current"));
+};
+
+const prevSlide = () => {
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add("current");
+  } else {
+    slides[slides.length - 1].classList.add("current");
+  }
+  setTimeout(() => current.classList.remove("current"));
+};
+
+prev.addEventListener("click", (e) => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+next.addEventListener("click", (e) => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+if (auto) {
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
+
+const year = document.querySelector(".year");
+year.innerHTML = new Date().getFullYear();
+
+const navBtn = document.querySelector(".bars-btn");
+const nav = document.querySelector(".nav");
+
+navBtn.addEventListener(
+  "click",
+  (e = () => {
+    nav.classList.toggle("showNav");
+  })
+);
